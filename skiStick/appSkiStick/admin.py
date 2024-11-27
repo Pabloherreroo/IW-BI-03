ec780cde211b9b26a909b4242adb7e0e634471db
Pablo Herrero <pabloherrero@opendeusto.es>
2024-11-27 20:11:57 +0100

    admin vista
    no consigo que funcione del todo

--- skiStick/appSkiStick/admin.py
+++ skiStick/appSkiStick/admin.py
@@ -1,14 +1,65 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.sites import AdminSite
 from .models import Estacion, Localizacion, TipoDePista, EstacionTipoDePista
 
-# Para editar los tipos de pista con cantidad directamente en el admin de Estacion añado este inline
 class EstacionTipoDePistaInline(admin.TabularInline):
     model = EstacionTipoDePista
     extra = 1
+
 class EstacionAdmin(admin.ModelAdmin):
     inlines = [EstacionTipoDePistaInline]
 
+    def has_view_permission(self, request, obj=None):
+        # Siempre permitir a superusuarios
+        if request.user.is_superuser:
+            return True
+        return request.user.has_perm("appSkiStick.view_estacion")
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return request.user.has_perm("appSkiStick.change_estacion")
+
+    def has_module_permission(self, request):
+        # Controla el módulo en la barra lateral
+        return self.has_view_permission(request)
+
+
+class TipoDePistaAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return request.user.has_perm("appSkiStick.view_tipodepista")
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return request.user.has_perm("appSkiStick.change_tipodepista")
+
+    def has_module_permission(self, request):
+        return self.has_view_permission(request)
+
 admin.site.register(Localizacion)
-admin.site.register(TipoDePista)
+admin.site.register(TipoDePista, TipoDePistaAdmin)
 admin.site.register(Estacion, EstacionAdmin)
 
+class CustomAdminSite(AdminSite):
+    site_header = "SkiStick Admin"
+    site_title = "SkiStick Admin Portal"
+    index_title = "Bienvenido al Portal de Administración SkiStick"
+
+    # Incluir el CSS personalizado
+    def get_urls(self):
+        from django.urls import path
+        urls = super().get_urls()
+        from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+        return urls + staticfiles_urlpatterns()
+
+    def each_context(self, request):
+        context = super().each_context(request)
+        context['css'] = '/static/admin/css/admin.css'
+        return context
+
+admin.site = CustomAdminSite()
--- skiStick/appSkiStick/apps.py
+++ skiStick/appSkiStick/apps.py
@@ -1,6 +1,38 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+class AppSkiStickConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appSkiStick"
 
-class AppskistickConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appSkiStick'
+    def ready(self):
+        # Conectar la señal post_migrate para crear roles después de la migración
+        post_migrate.connect(self.create_roles, sender=self)
+
+    def create_roles(self, **kwargs):
+        from django.contrib.auth.models import Group, Permission
+        from django.contrib.contenttypes.models import ContentType
+        from .models import Estacion, TipoDePista
+
+        grupos = {
+            "Admin Estaciones": ["change_estacion", "view_estacion"],
+            "Admin Tipos de Pista": ["change_tipodepista", "view_tipodepista"],
+        }
+
+        modelos = {
+            "change_estacion": Estacion,
+            "view_estacion": Estacion,
+            "change_tipodepista": TipoDePista,
+            "view_tipodepista": TipoDePista,
+        }
+
+        for grupo_nombre, permisos_codename in grupos.items():
+            group, _ = Group.objects.get_or_create(name=grupo_nombre)
+            for codename in permisos_codename:
+                modelo = modelos[codename]
+                content_type = ContentType.objects.get_for_model(modelo)
+                permission, _ = Permission.objects.get_or_create(
+                    codename=codename, content_type=content_type
+                )
+                group.permissions.add(permission)
+
