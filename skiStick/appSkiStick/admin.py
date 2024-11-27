075604636cbd6366b120e17531933d3795fb984d
Pablo Herrero <pabloherrero@opendeusto.es>
2024-11-27 20:57:07 +0100

    Arreglado CustomAdminSite y falta css

--- skiStick/appSkiStick/admin.py
+++ skiStick/appSkiStick/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group, User
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin.sites import AdminSite
 from .models import Estacion, Localizacion, TipoDePista, EstacionTipoDePista
+from django.templatetags.static import static
 
 class EstacionTipoDePistaInline(admin.TabularInline):
     model = EstacionTipoDePista
@@ -12,7 +11,6 @@ class EstacionAdmin(admin.ModelAdmin):
     inlines = [EstacionTipoDePistaInline]
 
     def has_view_permission(self, request, obj=None):
-        # Siempre permitir a superusuarios
         if request.user.is_superuser:
             return True
         return request.user.has_perm("appSkiStick.view_estacion")
@@ -23,7 +21,6 @@ class EstacionAdmin(admin.ModelAdmin):
         return request.user.has_perm("appSkiStick.change_estacion")
 
     def has_module_permission(self, request):
-        # Controla el módulo en la barra lateral
         return self.has_view_permission(request)
 
 
@@ -41,25 +38,19 @@ class TipoDePistaAdmin(admin.ModelAdmin):
     def has_module_permission(self, request):
         return self.has_view_permission(request)
 
-admin.site.register(Localizacion)
-admin.site.register(TipoDePista, TipoDePistaAdmin)
-admin.site.register(Estacion, EstacionAdmin)
-
 class CustomAdminSite(AdminSite):
     site_header = "SkiStick Admin"
     site_title = "SkiStick Admin Portal"
     index_title = "Bienvenido al Portal de Administración SkiStick"
 
-    # Incluir el CSS personalizado
-    def get_urls(self):
-        from django.urls import path
-        urls = super().get_urls()
-        from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-        return urls + staticfiles_urlpatterns()
-
     def each_context(self, request):
         context = super().each_context(request)
-        context['css'] = '/static/admin/css/admin.css'
+        context['custom_css'] = static('admin/css/admin.css')
         return context
 
-admin.site = CustomAdminSite()
+custom_admin_site = CustomAdminSite(name="custom_admin")
+
+custom_admin_site.register(Localizacion)
+custom_admin_site.register(TipoDePista, TipoDePistaAdmin)
+custom_admin_site.register(Estacion, EstacionAdmin)
+
--- skiStick/skiStick/settings.py
+++ skiStick/skiStick/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'skiStick.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "appSkiStick" / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
